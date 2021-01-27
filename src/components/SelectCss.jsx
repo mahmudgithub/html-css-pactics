2639a45c5e3faf8795f7be9a0606a3faa4cdafec
mahmudgithub <mahmudhossain838@gmail.com>
2021-01-27 05:43:54 -0800

    all kind of selector showed

--- src/components/SelectCss.jsx
+++ src/components/SelectCss.jsx
@@ -4,33 +4,40 @@ import React from 'react'
 const SelectCss = () => {
 
     return (
-<div>
+<div className='body'>
 
+{/* id selector example  */}
 <p id='one'>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, provident!
 </p>
 
+{/* class selector example */}
 <div>
     <p className='two'>
         Lorem ipsum dolor sit amet con
     </p>
 </div>
 
+
+{/* sub-attribte selector example*/}
 <div className='three'>
-    <p>Lorem </p>
+    <p className='sub-three'>sub three </p>
     <h1>Lorem ipsum dolor sit amet.</h1>
 </div>
 
-<div className='four'>
-    <div className='five'>
-        <div className='six'>
+{/* nested  id ettribute sector */}
+<div id='four'>
+    <div id='five'>
+        <div id='six'>
             <div>
                 <h2>i am inner </h2>
+                <h3>i am inner h3</h3>
             </div>
-
         </div>
     </div>
 </div>
+
+
 
 </div>
     )
