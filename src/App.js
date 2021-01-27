788042a277e397efd9cff39ba743f107d1e37e85
mahmudgithub <mahmudhossain838@gmail.com>
2021-01-27 07:51:04 -0800

    complited internal style

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
   return (
     <div className="App">
 {/* <SelectCss/> */}
- <InlineCss/>
- {/* <InternalCss/> */}
+ {/* <InlineCss/> */}
+ <InternalCss/>
  {/* <ExtarnalCss/> */}
     </div>
   );
--- src/components/InternalCss.jsx
+++ src/components/InternalCss.jsx
@@ -7,16 +7,63 @@ const InternalCss = () => {
         padding:"5px",
         margin:'15px',
         border:'solid'
+    };
+
+    var one={
+        backgroundColor:'green',
+        color:'white',
+        margin:'5px',
+        padding:'50px',
+        border:'solid black 5px'
+    };
+    var two={
+        border:'solid black 1px',
+        margin:'15px',
+        padding:'10px',
+        textAlign:'left',
+        color:'black',
+        backgroundColor:'blue',
+
+
+    };
+    var three={
+        border:'solid black 1px',
+        margin:'15px',
+        padding:'10px',
+        textAlign:'left',
+        color:'red',
+        backgroundColor:'black',
+
+    };
+    var four={
+        border:'solid black 1px',
+        margin:'15px',
+        padding:'10px',
+        textAlign:'left',
+        color:'blue',
+        backgroundColor:'red',
+
     }
 
-    // var one={
-    //     backgroundColor:blue;
-    // }
 
     return (
         <div>
             <div style={mystyle}> hello </div>
+            <div style={one}>Lorem ipsum dolor </div>
+
+
+
+                        <div style={two}> i am div two              
+                            <div style={three}> i am div three
+                                <div style={four}> i am div four
+                                </div>
+                                </div>
+                                </div>            
+                                
+                            
+                        
         </div>
+    
     )
 }
 
